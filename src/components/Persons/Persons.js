1fe9c95077621be934ca392e22bcfdbab52f1530
Andrey Kashapov <kashapovandrey@gmail.com>
2019-05-11 19:51:49 +0300

    shoudComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -13,7 +13,11 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("[Persons.js] shouldComponentUpdate", nextProps, nextState);
-    return true;
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
       { id: "asdf11", name: "Stephanie", age: 26 }
     ],
     otherState: "some other value",
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -32,7 +33,6 @@ class App extends Component {
     console.log("[App.js] componentDidMount");
     console.log("-----------------------------");
   }
-
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("[App.js] shouldComponentUpdate", nextProps, nextState);
@@ -89,12 +89,21 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
 
         {persons}
       </div>
