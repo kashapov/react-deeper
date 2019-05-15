d7cb80dbbe3fc93bba3bad3b9b03ea82689673ac
Andrey Kashapov <kashapovandrey@gmail.com>
2019-05-15 21:55:56 +0300

    hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import WithClass from "../../../hoc/WithClass";
+
 import classes from "./Person.css";
 
 class Person extends Component {
@@ -12,13 +14,17 @@ class Person extends Component {
       //throw new Error( 'Something went wrong' );
     }
     return (
-      <div className={classes.Person}>
+      <WithClass className={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </WithClass>
     );
   }
 }
